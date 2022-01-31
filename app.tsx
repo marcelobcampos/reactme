671b1097a4ed07692c78588c157fdf6d0da2e5a8
marceloc.dev <marceloc.dev@outlook.com>
2022-01-30 18:24:46 -0600

    remove counter comp from app

--- app.tsx
+++ app.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
-import Counter from './components/counter'
 import Myscene from './components/myscene'
 import FileData from './components/filedata'
 import ReadJson from './components/readjson'
@@ -8,7 +7,6 @@ import ReadJson from './components/readjson'
 const App = () => {
     return <div>
         <h2>React me!</h2>
-        <Counter />
         <FileData />
         <ReadJson />
     </div>
